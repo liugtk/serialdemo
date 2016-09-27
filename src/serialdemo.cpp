70083d50997f71aa4fc96adfc297cf47811a1af7
LIU Yuhui <yliu029@e.ntu.edu.sg>
2016-09-27 16:13:04 +0800

    just leave it here , use another branch to develop

--- src/serialdemo.cpp
+++ src/serialdemo.cpp
@@ -34,14 +34,14 @@ static float fcc_x = 0, fcc_y = 0, fcc_z = 0, fcc_xd = 0, fcc_yd = 0, fcc_zd = 0
 void print_sending_msg ();
 void print_sread();
 int receving_message();
-int add_Four(int a);
-int minus_Four(int a);
+int loop_add(int a);
+int loop_minus(int a);
 
 //********************Global variable define
 //********************variable define
 //********************freq
-const int single_loop_rate = 20;
-double rest_after_sync = single_loop_rate/1000.0 * 1.1;//half time of the loop 
+const int single_loop_rate = 40;
+double rest_after_sync = single_loop_rate/1000.0 * 1.1;//half time of the loop
 const int NodeNo = 3;
 
 //********************the global variable used
@@ -50,6 +50,7 @@ static bool updated = 0;
 static bool speaking = 0;
 static bool listening = 1;
 static bool listener_time_out = 0;
+static bool flag_timeout_timer = 0;
 //static bool time_reached = 0;
 static int package_loss_nu = 0;
 
@@ -58,7 +59,7 @@ static int package_loss_nu = 0;
 
 //********************Frame's initials
 #define local_MSG_LENGTH            32
-const double receve_time_out  = 32 * 0.002 + 0.01; // 2ms for each byte and 10 ms for extra wait
+const double receive_time_out  = 32 * 0.002 + 0.01; // 2ms for each byte and 10 ms for extra wait
 //define of read part
 static uint8_t swrite[local_MSG_LENGTH];
 static uint8_t sread[local_MSG_LENGTH];
@@ -113,8 +114,8 @@ int main(int argc, char **argv)
     ros::NodeHandle serialdemo_hdlr("~");
     //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
     ros::Rate loop_rate(single_loop_rate); // previous it is 40
-   
-    
+
+
     //FCC interfacing
     string fccSerialPort = string("/dev/ttyUSB0");
     //check for the FCC pot and the ID for the UAV
@@ -124,12 +125,12 @@ int main(int argc, char **argv)
         printf(KRED "Couldn't retrieve param 'fccSerialPort', using default port %s!\n"RESET, fccSerialPort.data());
     int ID_read;
     if (serialdemo_hdlr.getParam("ID", ID_read)){
-		printf(KBLU"Retrived ID = %d for param ID\n"RESET, ID_read );
-		local_ID = ID_read;
-	}else{
-		printf(KRED"can't retrive the ID, please try again.\n"RESET);
-		  exit (EXIT_FAILURE);
-	}
+        printf(KBLU"Retrived ID = %d for param ID\n"RESET, ID_read );
+        local_ID = ID_read;
+    }else{
+        printf(KRED"can't retrive the ID, please try again.\n"RESET);
+        exit (EXIT_FAILURE);
+    }
 
     fd.setPort(fccSerialPort.data());
     fd.setBaudrate(57600);
@@ -145,22 +146,26 @@ int main(int argc, char **argv)
         printf(KRED "serialInit: Failed to open port %s\n" RESET, fccSerialPort.data());
         return 0;
     }
-	//CREATE TIMER;
-	double startTime = (double)ros::Time::now().toSec();
-	double Time_loop = startTime;
-	double Time_loop2 = startTime;
-	int loop_count = 0;
-	
+    //CREATE TIMER;
+    double startTime = (double)ros::Time::now().toSec();
+    double Time_loop = startTime;
+    double Time_loop2 = startTime;
+    int loop_count = 0;
+    double receive_timer = 0;
 
-	
-	//variables define
-	int que_ID = minus_Four(local_ID);
-	O2H_ID = NodeNo - 1; // default for ID = 0 to work, need to receive NodeNo - 1
+
+    //variables define
+    int que_ID = loop_minus(local_ID);
+    O2H_ID = NodeNo - 1; // default for ID = 0 to work, need to receive NodeNo - 1
+     if ( O2H_ID == que_ID  )
+    {
+         flag_timeout_timer = 1;
+     }
     while (ros::ok())
     {
         double time = ros::Time::now().toSec(); // counting the time
         printf(KMAG"time used = %f \n"RESET, time - Time_loop );
-        Time_loop = time;     
+        Time_loop = time;
         //**********************************updating the infomation
         //generate sin functions
         float x = sin(time);//sin(2*PI*10*time);
@@ -190,158 +195,179 @@ int main(int argc, char **argv)
         }
         local_CS1 = CSA;
         local_CS2 = CSB;
+        //***************************end of Info Update
 
-	
-		while (listening){ // listen first
-			//static listen_timer = ros::Time::now().ToSec();
-			receving_message();
-			//print_sread();
-			if ( (O2H_ID == que_ID) && (updated || listener_time_out)  ){ // under receive ID = que_ID, if receive ID is updated or listener has time out, send again
-				speaking = 1;
-				listening = !speaking;
-				updated = 0;    // reset both back to 0 and wait for the next listen period.
-				listener_time_out = 0;
-				}
-		}
-		while(speaking){ //immediately talk after listen
-			fd.write(swrite, local_MSG_LENGTH);
-			//print_sending_msg();
-			speaking = 0;
-			listening = !speaking;
-		}
-		loop_count++;
 
-        	printf ("package loss numebr: %d, \n", package_loss_nu );
-		printf ("loops: %d, \n",loop_count);
-        	//loop_rate.sleep();
-	}
-    	return 0;
-  }
+        //Check buffer
+        if (fd.available() >= local_MSG_LENGTH) // >= enough bytes
+        {
+            //read
+            if (receving_message()) //only when message is successfully
+            {
+                //check ID
+                if ( (O2H_ID == que_ID)  )//receive the prece ID
+                {
+                    fd.write(swrite, local_MSG_LENGTH);
+                    print_sending_msg();
+                    //flag time_out on
+                    flag_timeout_timer = 1;
+                    receive_timer = ros::Time::now().toSec();
+                }
+                else if(O2H_ID == loop_add(local_ID)) // received next ID
+                {
+                    //flag time_out close
+                    flag_timeout_timer = 0;
+                }
+                else
+                {
+                    printf(KRED"incorrect ID received \n"RESET);
+                }
 
-int receving_message(){// idea to sync and receive message , at least receive a synced message or time_out
-	//sync
-	double receve_timer = ros::Time::now().toSec(); // counting the time
-	
-	while(1){
-		if (fd.available() >= local_MSG_LENGTH){ // >= enough bytes
-				while(!synced){ 
-					fd.read((uint8_T *)(sread + 0), 1);
-					//try sync
-					if (O2H_HEADER1 == 'U') {//UW the hearder	
-						fd.read((uint8_T *)(sread + 1), 1);
-						if(O2H_HEADER2 == 'W'){
-							// back up the previous data 
-							memcpy(sread_bak,sread, local_MSG_LENGTH );// dest: sread_bak, source: sread
-							fd.read((uint8_T *)(sread + 2),local_MSG_LENGTH -2 );
-							unsigned char CSA = 0, CSB = 0;
-							for (unsigned char i = 0; i < local_SUMCHECK_LENGTH; i++)
-							{
-								CSA = CSA + sread[2+i]; //4 - 31
-								CSB = CSB + CSA; // 32
-							}
-							printf (KYEL"the CSA = %d , CSB = %d, the recevied: CS1 = %d, CS2 = %d:"RESET, CSA, CSB, O2H_CS1, O2H_CS2 );
-							if (CSA == O2H_CS1 && CSB == O2H_CS2){
-								synced = true;
-								updated = true; // updated
-								//print_sread();// two of this in receve
-								printf (KGRN"synced\n"RESET);
-								return 1;
-							}else{ // failed, by default, synced = false, update = false
-								memcpy(sread + 2,sread_bak + 2, local_MSG_LENGTH - 2 );
-								break; // jump out
-								//no return, will try resync
-								receve_timer = ros::Time::now().toSec(); // update timer for another count
-							}
-						}//W
-					}//U
-					//after trying to sync, if buffer become empty
-					if (fd.available() == 0){ //break back to receive function, update the timer for time out
-						break; // jump out
-						//no return, will try resync
-						receve_timer = ros::Time::now().toSec();
-					}
-			}
-			if (synced){
-				memcpy(sread_bak,sread, local_MSG_LENGTH ); //back up sread to sread_bak
-				fd.read(sread, local_MSG_LENGTH);	
-				unsigned char CSA = 0, CSB = 0;
-				for (unsigned char i = 0; i < local_SUMCHECK_LENGTH; i++)
-				{
-						CSA = CSA + sread[2+i]; //4 - 31
-						CSB = CSB + CSA; // 32
-				}
-				printf (KYEL"the CSA = %d , CSB = %d, the recevied: CS1 = %d, CS2 = %d:"RESET, CSA, CSB, O2H_CS1, O2H_CS2 );
-				if (O2H_HEADER1 == 'U' && O2H_HEADER2 == 'W' && CSA == O2H_CS1 && CSB == O2H_CS2){ // check all 4 directly
-					// if correct, updated, return to main function and see if ID match the que
-					printf(KGRN"Matched\n"RESET);
-					updated = true; // updated
-					//print_sread();//two of this in receive
-					return 1;
-				}else{ // 1. restore, 2.package loss count, 3. synced turn off 4 update the time out and retry the receiving
-					printf("lost pakage, try sync again \n");
-					memcpy(sread + 2,sread_bak + 2, local_MSG_LENGTH - 2 );
-					package_loss_nu ++;
-					synced = false;
-					//no return, will check length again and try resync
-					receve_timer = ros::Time::now().toSec(); // update the timer for another count
-				}
-			}
-		}
-		// if not enough bety in the buffer
-		if ((ros::Time::now().toSec() - receve_timer) > receve_time_out){
-				listener_time_out = 1;
-				printf(KRED"time_out\n"RESET);
-				return -1;
-		}
-	}	// under while (1)
+            }
+
+        }
+        else if(flag_timeout_timer) //check time out when not enough data in buffer
+        {
+            if ((ros::Time::now().toSec() - receive_timer) > receive_time_out)
+            { // if timeout, resend, and reset timeout
+
+                printf(KRED"time_out\n"RESET);
+                //send
+                fd.write(swrite, local_MSG_LENGTH);
+                print_sending_msg();
+                //flag time_out on
+                flag_timeout_timer = 1;
+                receive_timer = ros::Time::now().toSec();
+            }
+        }
+
+        loop_count++;
+
+        printf ("package loss numebr: %d, \n", package_loss_nu );
+        printf ("loops: %d, \n",loop_count);
+        loop_rate.sleep();
+    }
+    return 0;
 }
 
-void print_sending_msg(){ // used for print msg for sending or local info
-		printf(KBLU"the sending message: ---------------------\n");
-        printf ("Header1 = %c\n", local_HEADER1);
-		printf ("Header2 = %c\n", local_HEADER2);
-		printf ("ID = %d\n", local_ID);
-		printf ("LENGTH = %d\n",local_LENGTH_BYTE);
-		printf ("x  = %f, y  = %f, z  = %f\n", local_X, local_Y, local_Z);
-		printf ("Dx = %f, Dy = %f, Dz = %f\n", local_DX, local_DY, local_DZ);
-		printf ("Nodecount = %d\n", local_NODECOUNT);
-		printf ("loss = %d\n", local_LOSS);
-		printf ("CS1 = %d, CS2 = %d\n"RESET, local_CS1, local_CS2);
-	}
+int receving_message(){
+    //sync
+    // already have enough bytes
+    while(!synced)
+    {
+        fd.read((uint8_T *)(sread + 0), 1);
+        //try sync
+        if (O2H_HEADER1 == 'U') {//UW the hearder
+            fd.read((uint8_T *)(sread + 1), 1);
+            if(O2H_HEADER2 == 'W'){
+                // back up the previous data
+                memcpy(sread_bak,sread, local_MSG_LENGTH );// dest: sread_bak, source: sread
+                fd.read((uint8_T *)(sread + 2),local_MSG_LENGTH -2 );
+                unsigned char CSA = 0, CSB = 0;
+                for (unsigned char i = 0; i < local_SUMCHECK_LENGTH; i++)
+                {
+                    CSA = CSA + sread[2+i]; //4 - 31
+                    CSB = CSB + CSA; // 32
+                }
+                printf (KYEL"the CSA = %d , CSB = %d, the recevied: CS1 = %d, CS2 = %d:"RESET, CSA, CSB, O2H_CS1, O2H_CS2 );
+                if (CSA == O2H_CS1 && CSB == O2H_CS2){
+                    synced = true;
+                    print_sread();// two of this in receive
+                    printf (KGRN"synced\n"RESET);
+                    return 1;
+                }else{ // failed, by default, synced = false, restore the memory, return -1 indicate unsuccessful
+                    memcpy(sread + 2,sread_bak + 2, local_MSG_LENGTH - 2 );
+                    return -1;
+                }
+            }//W
+        }//U
+        //after trying to sync, if buffer become empty
+        if (fd.available() == 0)
+        { // return -1 indicate unsuccessful receive
+            return -1;
+        }
+    }
+    if (synced)
+    {
+        memcpy(sread_bak,sread, local_MSG_LENGTH ); //back up sread to sread_bak
+        fd.read(sread, local_MSG_LENGTH);
+        unsigned char CSA = 0, CSB = 0;
+        for (unsigned char i = 0; i < local_SUMCHECK_LENGTH; i++)
+        {
+            CSA = CSA + sread[2+i]; //4 - 31
+            CSB = CSB + CSA; // 32
+        }
+        printf (KYEL"the CSA = %d , CSB = %d, the recevied: CS1 = %d, CS2 = %d:"RESET, CSA, CSB, O2H_CS1, O2H_CS2 );
+        if (O2H_HEADER1 == 'U' && O2H_HEADER2 == 'W' && CSA == O2H_CS1 && CSB == O2H_CS2){ // check all 4 directly
+            // if correct, updated, return to main function and see if ID match the que
+            printf(KGRN"Matched\n"RESET);
+            print_sread();//two of this in receive
+            return 1;
+        }
+        else
+        { // 1. restore, 2.package loss count, 3. synced turn off 4. return -1 to indicate the unsuccessful receive
+            printf("lost pakage, try sync again \n");
+            memcpy(sread + 2,sread_bak + 2, local_MSG_LENGTH - 2 );
+            package_loss_nu ++;
+            synced = false;
+            return -1;
 
-void print_sread(){ //used for print readed msg from others
-	    printf ("the received message: ************************\n");
-        printf ("Header1 = %c\n", O2H_HEADER1);
-		printf ("Header2 = %c\n", O2H_HEADER2);
-		printf ("ID = %d\n", O2H_ID);
-		printf ("LENGTH = %d\n",O2H_LENGTH_BYTE);
-		printf ("x  = %f, y  = %f, z  = %f\n", O2H_X, O2H_Y, O2H_Z);
-		printf ("Dx = %f, Dy = %f, Dz = %f\n", O2H_DX, O2H_DY, O2H_DZ);
-		printf ("Nodecount = %d\n", O2H_NODECOUNT);
-		printf ("loss = %d\n", O2H_LOSS);
-		printf ("CS1 = %d, CS2 = %d\n", O2H_CS1, O2H_CS2);
-	}
-	
-int add_Four(int a){ // loop inside 0,1,2,3... NodeNo -1
-	if (a < 0 || a > (NodeNo -1)){
-		printf(KRED"use a invalid input in the function add_four"RESET);
-		}
-		if (a == (NodeNo -1)){
-			return 0;
-		}else{
-			return a + 1;	
-	}
+        }
+    }
+
 }
-int minus_Four(int a){ // loop inside 0,1,2,3 .... NodeNo -1
-	if (a < 0 || a > (NodeNo -1)){
-		printf(KRED"use a invalid input in the function add_four"RESET);
-		}
-		if (a == 0){
-			return (NodeNo -1 );
-		}else{
-			return a - 1;	
-	}
+
+void print_sending_msg()
+{ // used for print msg for sending or local info
+    printf(KBLU"the sending message: ---------------------\n");
+    printf ("Header1 = %c\n", local_HEADER1);
+    printf ("Header2 = %c\n", local_HEADER2);
+    printf ("ID = %d\n", local_ID);
+    printf ("LENGTH = %d\n",local_LENGTH_BYTE);
+    printf ("x  = %f, y  = %f, z  = %f\n", local_X, local_Y, local_Z);
+    printf ("Dx = %f, Dy = %f, Dz = %f\n", local_DX, local_DY, local_DZ);
+    printf ("Nodecount = %d\n", local_NODECOUNT);
+    printf ("loss = %d\n", local_LOSS);
+    printf ("CS1 = %d, CS2 = %d\n"RESET, local_CS1, local_CS2);
 }
+
+void print_sread()
+{ //used for print readed msg from others
+    printf ("the received message: ************************\n");
+    printf ("Header1 = %c\n", O2H_HEADER1);
+    printf ("Header2 = %c\n", O2H_HEADER2);
+    printf ("ID = %d\n", O2H_ID);
+    printf ("LENGTH = %d\n",O2H_LENGTH_BYTE);
+    printf ("x  = %f, y  = %f, z  = %f\n", O2H_X, O2H_Y, O2H_Z);
+    printf ("Dx = %f, Dy = %f, Dz = %f\n", O2H_DX, O2H_DY, O2H_DZ);
+    printf ("Nodecount = %d\n", O2H_NODECOUNT);
+    printf ("loss = %d\n", O2H_LOSS);
+    printf ("CS1 = %d, CS2 = %d\n", O2H_CS1, O2H_CS2);
+}
+
+int loop_add(int a)
+{ // loop inside 0,1,2,3... NodeNo -1
+    if (a < 0 || a > (NodeNo -1)){
+        printf(KRED"use a invalid input in the function loop_add"RESET);
+    }
+    if (a == (NodeNo -1)){
+        return 0;
+    }else{
+        return a + 1;
+    }
+}
+int loop_minus(int a)
+{ // loop inside 0,1,2,3 .... NodeNo -1
+    if (a < 0 || a > (NodeNo -1)){
+        printf(KRED"use a invalid input in the function small_add"RESET);
+    }
+    if (a == 0){
+        return (NodeNo -1 );
+    }else{
+        return a - 1;
+    }
+}
+
 
 
 
