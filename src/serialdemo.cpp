562d720268af19823292eb0ca4399a76d5c4bda6
LIU Yuhui <yliu029@e.ntu.edu.sg>
2016-12-06 13:27:23 +0800

    for test the digimesh in broadcast by 64 Mac addr

--- src/serialdemo.cpp
+++ src/serialdemo.cpp
@@ -40,7 +40,7 @@ int loop_minus(int a);
 //********************Global variable define
 //********************variable define
 //********************freq
-static int single_loop_rate = 20;
+static int single_loop_rate = 5;
 double rest_after_sync = single_loop_rate/1000.0 * 1.1;//half time of the loop
 static int NodeNo = 3;
 
@@ -53,55 +53,70 @@ static bool listener_time_out = 0;
 static bool flag_timeout_timer = 0;
 //static bool time_reached = 0;
 static int package_loss_nu = 0;
-
+static double receive_time_out  = 32 * 0.002 + 0.01; // 2ms for each byte and 10 ms for extra wait
 
 
 
 //********************Frame's initials
-#define local_MSG_LENGTH            32
-static double receive_time_out  = 32 * 0.002 + 0.01; // 2ms for each byte and 10 ms for extra wait
+
+
+#define local_MSG_LENGTH            43
+
 //define of read part
-static uint8_t swrite[local_MSG_LENGTH];
-static uint8_t sread[local_MSG_LENGTH];
-static uint8_t sread_bak[local_MSG_LENGTH];
-//*******************OUT to Here : O2H
-#define O2H_HEADER1               (*(uint8_T *)(sread + 0)) //'U'
-#define O2H_HEADER2               (*(uint8_T *)(sread + 1)) //'W'
+static uint8_T swrite[local_MSG_LENGTH];
 
-#define O2H_ID                    (*(uint8_T *)(sread + 2))
-#define O2H_LENGTH_BYTE           (*(uint8_T *)(sread + 3))
-#define O2H_X                     (*(float *)(sread + 4))
-#define O2H_Y                     (*(float *)(sread + 8))
-#define O2H_Z                     (*(float *)(sread + 12))
-#define O2H_DX                    (*(float *)(sread + 16))
-#define O2H_DY                    (*(float *)(sread + 20))
-#define O2H_DZ                    (*(float *)(sread + 24))
-#define O2H_NODECOUNT             (*(uint8_T *)(sread + 28))
-#define O2H_LOSS                  (*(uint8_T *)(sread + 29))
 
-#define O2H_CS1                   (*(uint8_T *)(sread + 30))
-#define O2H_CS2                   (*(uint8_T *)(sread + 31))
 
-//*************************define of sending part
+#define local_SD          		    (*(uint8_T *)(swrite + 0)) //0x7E
+#define local_Length1               (*(uint16_T *)(swrite + 1)) // 1.2   0xXX XX total bytes - 4 (most likely, start delimiter(1), length(2) and checksum(1) ),  42 (add the 0) - 4=38 = 0x26
+#define local_Length2               (*(uint16_T *)(swrite + 2))
+#define local_FrameType          	(*(uint8_T *)(swrite + 3)) // 3  0x10 Transmit Request
+#define local_FrameID				(*(uint8_T *)(swrite + 4)) // 4 0x00  or other number use 00 means no response
 
-#define local_HEADER1               (*(uint8_T *)(swrite + 0)) //'U'
-#define local_HEADER2               (*(uint8_T *)(swrite + 1)) //'W'
+#define local_64Addr1				(*(uint32_T *)(swrite + 5)) //5678 9abc 5 to 8
+#define local_64Addr2               (*(uint32_T *)(swrite + 9)) //5678 9abc 9 to 12
+#define local_16Addr				(*(uint16_T *)(swrite + 13)) // 13,14 0xFF FE
+#define local_Broadcast_r			(*(uint8_T *)(swrite + 15)) // 15, 0x00 00 max hop times
+#define local_Options				(*(uint8_T *)(swrite + 16)) //16 0x00 by default
+//RF data
+#define local_ID                    (*(uint8_T *)(swrite + 17)) // 17
+#define local_X                     (*(float *)(swrite + 18)) //17 18 19 20 + 1
+#define local_Y                     (*(float *)(swrite + 22)) //21 22 23 24 +1
+#define local_Z                     (*(float *)(swrite + 26)) //25 26 27 28 +1
+#define local_DX                    (*(float *)(swrite + 30)) //29 30 31 32 +1
+#define local_DY                    (*(float *)(swrite + 34)) //33 34 35 36 +1
+#define local_DZ                    (*(float *)(swrite + 38)) //37 38 39 40 +1
 
-#define local_ID                    (*(uint8_T *)(swrite + 2))
-#define local_LENGTH_BYTE           (*(uint8_T *)(swrite + 3))
-#define local_X                     (*(float *)(swrite + 4))
-#define local_Y                     (*(float *)(swrite + 8))
-#define local_Z                     (*(float *)(swrite + 12))
-#define local_DX                    (*(float *)(swrite + 16))
-#define local_DY                    (*(float *)(swrite + 20))
-#define local_DZ                    (*(float *)(swrite + 24))
-#define local_NODECOUNT             (*(uint8_T *)(swrite + 28))
-#define local_LOSS                  (*(uint8_T *)(swrite + 29))
+#define local_CS                   (*(uint8_T *)(swrite + 42)) //42
 
-#define local_CS1                   (*(uint8_T *)(swrite + 30))
-#define local_CS2                   (*(uint8_T *)(swrite + 31))
-#define local_SUMCHECK_LENGTH       28
-#define local_DATA_LENGTH           26
+#define local_SUMCHECK_LENGTH       39  // from the Frame type 3 to local_DZ 40
+#define local_length                0x27
+
+//define of the read port
+#define receive_MSG_LENGTH         41
+// don have the Frame ID and the broadcast_r    O2H -> out 2 here
+static uint8_T sread[receive_MSG_LENGTH];
+static uint8_T sread_bak[receive_MSG_LENGTH];
+#define O2H_SD          		    (*(uint8_T *)(sread + 0)) //0x7E
+#define O2H_Length1                 (*(uint8_T *)(sread + 1)) // 1.2   0xXX XX total bytes - 4 (most likely, start delimiter(1), length(2) and checksum(1) ),  40 (add the 0) - 4=36 = 0x0024
+#define O2H_Length2                 (*(uint8_T *)(sread + 2))
+#define O2H_FrameType           	(*(uint8_T *)(sread + 3)) // 3  0x90 Transmit Request
+#define O2H_64Addr1             	(*(uint32_T *)(sread + 4)) //4-7
+#define O2H_64Addr2                 (*(uint32_T *)(sread + 8)) // 8-11
+#define O2H_16Addr                  (*(uint16_T *)(sread + 12)) // 12,13 0xFF FE
+#define O2H_Options             	(*(uint8_T *)(sread + 14)) //14 0xC1
+//RF data
+#define O2H_ID                      (*(uint8_T *)(sread + 15)) // 15
+#define O2H_X                       (*(float *)(sread + 16)) //15 16 17 18 + 1
+#define O2H_Y                       (*(float *)(sread + 20)) //19 20 21 22 +1
+#define O2H_Z                       (*(float *)(sread + 24)) //23 24 25 26 +1
+#define O2H_DX                      (*(float *)(sread + 28)) //27 28 29 30 +1
+#define O2H_DY                      (*(float *)(sread + 32)) //31 32 33 34 +1
+#define O2H_DZ                      (*(float *)(sread + 36)) //35 36 37 38 +1
+
+#define O2H_CS                      (*(uint8_T *)(sread + 40)) //40
+
+#define O2H_SUMCHECK_LENGTH       37  // from the Frame type 3 to local_DZ 38
 
 using namespace std;
 
@@ -113,7 +128,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "serialdemo");
     ros::NodeHandle serialdemo_hdlr("~");
     //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
-    ros::Rate loop_rate(single_loop_rate); // previous it is 40
+
 
 
     //FCC interfacing
@@ -156,7 +171,7 @@ int main(int argc, char **argv)
         printf(KRED"can't retrive the timeout_read, please try again.\n"RESET);
         exit (EXIT_FAILURE);
     }
-
+    ros::Rate loop_rate(single_loop_rate); // previous it is 40
     fd.setPort(fccSerialPort.data());
     fd.setBaudrate(57600);
     fd.setTimeout(5, 10, 2, 10, 2);
@@ -174,7 +189,7 @@ int main(int argc, char **argv)
     //CREATE TIMER;
     double startTime = (double)ros::Time::now().toSec();
     double Time_loop = startTime;
-    double Time_loop2 = startTime;
+
     int loop_count = 0;
     double receive_timer = 0;
 
@@ -198,28 +213,29 @@ int main(int argc, char **argv)
         float z = sin(time/10);//sin(2*PI*10*time + 3*PI/2);
 
         //populate the header values
-        local_HEADER1 = 'U';
-        local_HEADER2 = 'W';
-        //local_ID = 0; defeined at the top when retriving ID
-        local_LENGTH_BYTE = local_DATA_LENGTH;
+
+        local_SD = 0x7E;
+        local_Length1 = 0x00;
+        local_Length2 = local_length;
+        local_FrameType = 0x10;
+        local_FrameID = 0x00; // no feed back to the sender
+        local_64Addr1 = 0x00000000; // broadcast to all Mac address
+        local_64Addr2 = 0xFFFF0000; // remenber to swap.
+        local_16Addr = 0xFEFF;       // remember to swap.
+        local_Broadcast_r = 0x00;
+        local_Options = 0x00;
         local_X = x;
         local_Y = y;
         local_Z = z;
-        local_DX = y;
-        local_DY = -x;
-        local_DZ = y/10;
-        local_NODECOUNT = 4;
-        local_LOSS = 0;
-
-        //load the crc
-        unsigned char CSA = 0, CSB = 0;
+        local_DX = x;
+        local_DY = y;
+        local_DZ = z;
+        unsigned char CSA = 0;
         for (unsigned char i = 0; i < local_SUMCHECK_LENGTH; i++)
         {
-            CSA = CSA + swrite[2+i]; //4 - 31
-            CSB = CSB + CSA; // 32
+            CSA = CSA + swrite[3+i]; //3 - 40
         }
-        local_CS1 = CSA;
-        local_CS2 = CSB;
+        local_CS = 0xFF - CSA;
         //***************************end of Info Update
 
 
@@ -227,13 +243,19 @@ int main(int argc, char **argv)
         if (fd.available() >= local_MSG_LENGTH) // >= enough bytes
         {
             //read
+            //printf("test1, in avail()");
             if (receving_message()) //only when message is successfully
             {
+                //printf("test2, in receive");
                 //check ID
                 if ( (O2H_ID == que_ID)  )//receive the prece ID
                 {
                     fd.write(swrite, local_MSG_LENGTH);
                     print_sending_msg();
+                    for (int i = 0; i < local_MSG_LENGTH; i++)
+                    {
+                        printf("%02x ", swrite[i]);
+                    }
                     //flag time_out on
                     flag_timeout_timer = 1;
                     receive_timer = ros::Time::now().toSec();
@@ -260,7 +282,11 @@ int main(int argc, char **argv)
                 //send
                 fd.write(swrite, local_MSG_LENGTH);
                 print_sending_msg();
-                //flag time_out on
+                for (int i = 0; i < local_MSG_LENGTH; i++)
+                {
+                    printf("%02x ", swrite[i]);
+                }
+
                 flag_timeout_timer = 1;
                 receive_timer = ros::Time::now().toSec();
             }
@@ -282,30 +308,29 @@ int receving_message(){
     {
         fd.read((uint8_T *)(sread + 0), 1);
         //try sync
-        if (O2H_HEADER1 == 'U') {//UW the hearder
-            fd.read((uint8_T *)(sread + 1), 1);
-            if(O2H_HEADER2 == 'W'){
-                // back up the previous data
-                memcpy(sread_bak,sread, local_MSG_LENGTH );// dest: sread_bak, source: sread
-                fd.read((uint8_T *)(sread + 2),local_MSG_LENGTH -2 );
-                unsigned char CSA = 0, CSB = 0;
-                for (unsigned char i = 0; i < local_SUMCHECK_LENGTH; i++)
-                {
-                    CSA = CSA + sread[2+i]; //4 - 31
-                    CSB = CSB + CSA; // 32
-                }
-                printf (KYEL"the CSA = %d , CSB = %d, the recevied: CS1 = %d, CS2 = %d:"RESET, CSA, CSB, O2H_CS1, O2H_CS2 );
-                if (CSA == O2H_CS1 && CSB == O2H_CS2){
-                    synced = true;
-                    print_sread();// two of this in receive
-                    printf (KGRN"synced\n"RESET);
-                    return 1;
-                }else{ // failed, by default, synced = false, restore the memory, return 0 indicate unsuccessful
-                    memcpy(sread + 2,sread_bak + 2, local_MSG_LENGTH - 2 );
-                    return 0;
-                }
-            }//W
-        }//U
+        if (O2H_SD == 0x7E)    // SD = 7E
+        {
+            // back up the previous data
+            memcpy(sread_bak,sread, local_MSG_LENGTH );// dest: sread_bak, source: sread
+            fd.read((uint8_T *)(sread + 1),receive_MSG_LENGTH - 1 );
+            unsigned char CSA = 0;
+            for (unsigned char i = 0; i < O2H_SUMCHECK_LENGTH; i++)
+            {
+                CSA = CSA + sread[3+i]; //3 - 38
+            }
+            CSA = 0xFF - CSA;
+            printf (KYEL"the CSA = %d , the recevied: CS1 = %d"RESET, CSA, O2H_CS );
+            if (CSA == O2H_CS){
+                synced = true;
+                print_sread();// two of this
+                printf (KGRN"synced\n"RESET);
+                return 1;
+            }else{ // failed, by default, synced = false, restore the memory, return 0 indicate unsuccessful
+                memcpy(sread ,sread_bak , receive_MSG_LENGTH );
+                return 0;
+            }
+
+        }//SD
         //after trying to sync, if buffer become empty
         if (fd.available() == 0)
         { // return 0 indicate unsuccessful receive
@@ -316,14 +341,14 @@ int receving_message(){
     {
         memcpy(sread_bak,sread, local_MSG_LENGTH ); //back up sread to sread_bak
         fd.read(sread, local_MSG_LENGTH);
-        unsigned char CSA = 0, CSB = 0;
-        for (unsigned char i = 0; i < local_SUMCHECK_LENGTH; i++)
+        unsigned char CSA = 0;
+        for (unsigned char i = 0; i < O2H_SUMCHECK_LENGTH; i++)
         {
-            CSA = CSA + sread[2+i]; //4 - 31
-            CSB = CSB + CSA; // 32
+            CSA = CSA + sread[3+i]; //3 - 38
         }
-        printf (KYEL"the CSA = %d , CSB = %d, the recevied: CS1 = %d, CS2 = %d:"RESET, CSA, CSB, O2H_CS1, O2H_CS2 );
-        if (O2H_HEADER1 == 'U' && O2H_HEADER2 == 'W' && CSA == O2H_CS1 && CSB == O2H_CS2){ // check all 4 directly
+        CSA = 0xFF - CSA;
+        printf (KYEL"the CSA = %d , the recevied: CS1 = %d\n"RESET, CSA, O2H_CS );
+        if (CSA == O2H_CS && O2H_SD == 0x7E){ // check both 2 directly
             // if correct, updated, return to main function and see if ID match the que
             printf(KGRN"Matched\n"RESET);
             print_sread();//two of this in receive
@@ -332,7 +357,7 @@ int receving_message(){
         else
         { // 1. restore, 2.package loss count, 3. synced turn off 4. return 0 to indicate the unsuccessful receive
             printf("lost pakage, try sync again \n");
-            memcpy(sread + 2,sread_bak + 2, local_MSG_LENGTH - 2 );
+             memcpy(sread ,sread_bak , receive_MSG_LENGTH  );
             package_loss_nu ++;
             synced = false;
             return 0;
@@ -345,30 +370,27 @@ int receving_message(){
 void print_sending_msg()
 { // used for print msg for sending or local info
     printf(KBLU"the sending message: ---------------------\n");
-    printf ("Header1 = %c\n", local_HEADER1);
-    printf ("Header2 = %c\n", local_HEADER2);
+    printf ("Header = %c\n", local_SD);
     printf ("ID = %d\n", local_ID);
-    printf ("LENGTH = %d\n",local_LENGTH_BYTE);
     printf ("x  = %f, y  = %f, z  = %f\n", local_X, local_Y, local_Z);
     printf ("Dx = %f, Dy = %f, Dz = %f\n", local_DX, local_DY, local_DZ);
-    printf ("Nodecount = %d\n", local_NODECOUNT);
-    printf ("loss = %d\n", local_LOSS);
-    printf ("CS1 = %d, CS2 = %d\n"RESET, local_CS1, local_CS2);
+    printf ("CS = %d\n"RESET, local_CS);
 }
 
 void print_sread()
 { //used for print readed msg from others
     printf ("the received message: ************************\n");
-    printf ("Header1 = %c\n", O2H_HEADER1);
-    printf ("Header2 = %c\n", O2H_HEADER2);
+    printf ("Header = %c\n", O2H_SD);
+
     printf ("ID = %d\n", O2H_ID);
-    printf ("LENGTH = %d\n",O2H_LENGTH_BYTE);
+
     printf ("x  = %f, y  = %f, z  = %f\n", O2H_X, O2H_Y, O2H_Z);
     printf ("Dx = %f, Dy = %f, Dz = %f\n", O2H_DX, O2H_DY, O2H_DZ);
-    printf ("Nodecount = %d\n", O2H_NODECOUNT);
-    printf ("loss = %d\n", O2H_LOSS);
-    printf ("CS1 = %d, CS2 = %d\n", O2H_CS1, O2H_CS2);
+
+
+    printf ("CS = %d\n"RESET, O2H_CS);
 }
+
 
 int loop_add(int a)
 { // loop inside 0,1,2,3... NodeNo -1
